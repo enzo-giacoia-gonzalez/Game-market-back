707e65ea6c820834b1654d25f2b70f423de2a6fc
EVZODA <enzogiacoia@hotmail.com>
2023-02-13 17:43:53 -0300

    ok

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 
 
-const validarCampos = require('../middlewares/validar-campos');
+const validaCampos = require('../middlewares/validar-campos');
 const validarJWT = require('../middlewares/validar-jwt');
 const validaRoles = require('../middlewares/validar-roles');
 
 module.exports = {
-    ...validarCampos,
+    ...validaCampos,
     ...validarJWT,
     ...validaRoles,
 }
